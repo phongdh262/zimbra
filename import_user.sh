5ee183bd45b68fa708ba205bd7980226cd9ab12e
phongdh262 <120239699+phongdh262@users.noreply.github.com>
2022-12-12 22:44:24 +0700

    Update import_user.sh

--- import_user.sh
+++ import_user.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
-# Usage: import_user.sh user.csv
+#Usage: import_user.sh user.csv
 file=$1
-
-while read line;
-do
+while read line; do
 Username=`echo $line | cut -d"," -f1 |  tr -d '\r'`
 Password=`echo $line | cut -d"," -f2 |  tr -d '\r'`
 zmprov ca $Username $Password
