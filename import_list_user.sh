f18633de74d228a96e9359c12aee309c34ac0fe6
phongdh262 <120239699+phongdh262@users.noreply.github.com>
2022-12-12 22:01:12 +0700

    Update import_list_user.sh

--- import_list_user.sh
+++ import_list_user.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
 # Usage: import_user.sh user.csv
 file=$1
+
 while read line; do
+#Dept=`echo $line | cut -d"," -f1 |  tr -d '\r'`
 Username=`echo $line | cut -d"," -f1 |  tr -d '\r'`
 Password=`echo $line | cut -d"," -f2 |  tr -d '\r'`
-zmprov ca $Username $Password 
+zmprov ca $Username $Password
+#title $Dept
 echo "Create user $Username"
 done < $file
